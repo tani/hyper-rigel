7db3031b6620ad303c55ef988d4db314c6e4da5a
TANIGUCHI Masaya <ta2gch@gmail.com>
2021-10-26 01:28:01 +0900

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// Original https://github.com/Rigellute/rigel licensed under the MIT License
+// Copyright (c) 2021 TANIGUCHI Masaya. All rights reserved.
+// This work is licensed under the MIT License. https://git.io/mit-license
+
+exports.decorateConfig = (config) => ({
+  ...config,
+  foregroundColor: "#e6e6dc",
+  backgroundColor: "#002635",
+  cursorColor: "#e6e6dc",
+  cursorAccentColor: "#002635",
+  selectionColor: "#194b5e",
+  colors: {
+    black: "#00384d",
+    red: "#c43060",
+    green: "#7fc06e",
+    yellow: "#f08e48",
+    blue: "#1c8db2",
+    magenta: "#c694ff",
+    cyan: "#00cccc",
+    white: "#77929e",
+    lightBlack: "#517f8d",
+    lightRed: "#ff5a67",
+    lightGreen: "#9cf087",
+    lightYellow: "#ffcc1b",
+    lightBlue: "#7eb2dd",
+    lightMagenta: "#fb94ff",
+    lightCyan: "#00ffff",
+    lightWhite: "#b7cff9",
+  },
+});
